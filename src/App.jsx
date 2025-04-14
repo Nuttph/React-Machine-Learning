a5809e3e0130e0eb1e56e248f27ab9f46ac92fa2
Nuttaphon <nathn2420@gmail.com>
2025-04-14 19:50:37 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import HandDetector from './components/HandDetector'
 import TextToVideo from './components/texttovideo/TextToVideo'
 
+import { Routes,Route, Link } from 'react-router-dom'
+
 const App = () => {
   return (
     <div className=''>
-      {/* <HandDetector/> */}
-      <TextToVideo/>
+      <div className='flex flex-row items-center justify-center gap-4'>
+        <Link className='px-4 py-2 bg-yellow-300 hover:bg-yellow-600 duration-300 rounded-lg border my-4' to={"/hand-detect"}>Hand Detect</Link>
+        <Link className='px-4 py-2 bg-yellow-300 hover:bg-yellow-600 duration-300 rounded-lg border my-4' to={"/text-to-video"}>Text to Video</Link>
+      </div>
+      <Routes>
+        <Route path="/hand-detect" element={<HandDetector/>}/>
+        <Route path="/text-to-video" element={<TextToVideo/>}/>
+      </Routes>
     </div>
   )
 }
